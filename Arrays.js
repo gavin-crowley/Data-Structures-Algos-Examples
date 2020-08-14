193d4681a586d1bdfac9431c62df0c14957b1f8c
gavin-crowley <42537387+gavin-crowley@users.noreply.github.com>
2020-08-14 15:03:48 +0400

    Aug 14

--- Arrays.js
+++ Arrays.js
@@ -7,7 +7,7 @@ daysOfWeek = new Array('Sunday', 'Monday', 'Tuesday');
 let daysOfWeek = [];
 let daysOfWeek = ['Sunday', 'Monday', 'Tuesday']
 
-
+//asdasdasdasd
 
 // Accessing elements and iterating an array
 
